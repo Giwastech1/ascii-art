f998c76557e692c81e1946dbe04f6015e4e6f53d
giwastech1 <giwatoheeb27@gmail.com>
2026-03-28 15:59:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project Name: Ascii Art
+
+**Objective:**
+```
+Ascii-art is a program which consists in receiving a string as an argument and outputting the string in a graphic representation using ASCII. Time to write big.
+```
+
+Note: Thus.
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"ascii-art/utils"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Error: Expected atleast 2 arguments")
+		return
+	}
+
+	input := os.Args[1]
+
+	output := utils.AsciiArt(input)
+	fmt.Print(output)
+}
